91f33b25c7493edd23bd1d41a0beb2726a98acf9
ArmandoDiaz <armando.diaz.diaz.2409@gmail.com>
2023-05-10 22:00:59 -0600

    bug agregar tesoros

--- src/app/modules/pages/tesoros/tesoros.component.ts
+++ src/app/modules/pages/tesoros/tesoros.component.ts
@@ -46,7 +46,7 @@ export class TesorosComponent implements OnInit {
       alert('Completa toda la información');
     }
 
-    if(this.frmTesoro.value.id == "" ){
+    if(this.frmTesoro.value.id){
       this.agregarTesoro();
     }else{
       this.editarTesoro();
